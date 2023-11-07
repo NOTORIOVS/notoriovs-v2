d7c446e89bdea96b3349dda4f2d54b531ad2c6ad
Adam Black <fernando@notoriovs.com>
2023-11-06 21:57:38 -0600

    Fixed: Header Rendering On Load

--- src/components/header.js
+++ src/components/header.js
@@ -8,24 +8,23 @@ import { usePathname } from 'next/navigation';
 
 export default function Header() {
   const [openBurger, setOpenBurger] = useState(false);
-  const [ww, setWindowWidth] = useState(414);
+  const [ww, setWindowWidth] = useState(undefined);
   const pathName = usePathname();
-
 
   useEffect(() => {
     const updateWidth = () => {
-      setWindowWidth(window.innerWidth)
-    }
-    window.addEventListener('load', updateWidth);
+      setWindowWidth(window.innerWidth);
+    };
+    updateWidth();
     window.addEventListener('resize', updateWidth);
-    return () => window.removeEventListener('resize', updateWidth)
-  })
+    return () => window.removeEventListener('resize', updateWidth);
+  }, []);
 
-  const mainHeader = () => (
-    <div className="hidden lg:flex fixed h-screen z-[999]">
-      <div className="fixed flex flex-col h-full flex-grow w-[20rem] left-0 p-4">
+  if (pathName === '/survey') {
+    return (
+      <div className="fixed z-[9999] flex justify-center right-0 left-0 p-4 bg-black border-b border-white">
         <Link href="/">
-          <div id="logo" className="flex items-center w-full h-12 relative top-0">
+          <div id="logo" className="flex items-center w-[20rem] h-12 relative top-0 invert">
             <Img
               src={logotype}
               fill={true}
@@ -33,64 +32,56 @@ export default function Header() {
             />
           </div>
         </Link>
-        <div
-          id="headerFoot"
-          className="mt-auto"
-        >
-          <p className="-ft-3">English // Spanish</p>
-        </div>
       </div>
-      <div className="fixed flex flex-col h-full flex-grow w-[20rem] right-0 p-4 items-end space-y-2">
-        <a href={`https://instagram.com/${info.social.instagram}`} className="-ft-3" target="_blank">Instagram</a>
-        <a href={`https://facebook.com/${info.social.facebook}`} className="-ft-3" target="_blank">Facebook</a>
-        <a href={`https://wa.me/${info.whatsapp.value}`} className="-ft-3" target="_blank">WhatsApp</a>
-      </div>
-    </div>
-  );
-
-  const mobileHeader = () => (
-    <div className="fixed top-0 inset-x-0 flex lg:hidden justify-between bg-white border-b p-4 z-[9999]">
-      <Link href="/">
-        <div id="logo" className="relative flex justify-start w-[16rem] pt-[4rem] top-0">
-          <Img
-            src={logotype}
-            fill={true}
-            style={{objectFit: 'contain', filter: 'brightness(0)'}}
-          />
-        </div>
-      </Link>
-      <div className="w-[4rem] h-[4rem] cursor-pointer" onClick={() => setOpenBurger(!openBurger)}>
-        <a
-          className={`burger_box w-10 h-8 ${openBurger && 'active'}`}
-        >
-          <span className="burger_ico"/>
-        </a>
-      </div>
-    </div>
-  );
-
-  const simpleHeader = () => (
-    <div className="fixed z-[9999] flex justify-center right-0 left-0 p-4 bg-black border-b border-white">
-      <Link href="/">
-        <div id="logo" className="flex items-center w-[20rem] h-12 relative top-0 invert">
-          <Img
-            src={logotype}
-            fill={true}
-            style={{objectFit: 'contain'}}
-          />
-        </div>
-      </Link>
-    </div>
-  )
-
-  if (pathName === '/survey') {
-    return simpleHeader();
-  }
-  if (ww <= 1023) {
-    // return mobileHeader();
-    return simpleHeader();
+    );
   }
 
-  return mainHeader();
+  return (
+    <>
+      <div className="hidden lg:flex fixed h-screen z-[999]">
+        <div className="fixed flex flex-col h-full flex-grow w-[20rem] left-0 p-4">
+          <Link href="/">
+            <div id="logo" className="flex items-center w-full h-12 relative top-0">
+              <Img
+                src={logotype}
+                fill={true}
+                style={{objectFit: 'contain'}}
+              />
+            </div>
+          </Link>
+          <div
+            id="headerFoot"
+            className="mt-auto"
+          >
+            <p className="-ft-3">English // Spanish</p>
+          </div>
+        </div>
+        <div className="fixed flex flex-col h-full flex-grow w-[20rem] right-0 p-4 items-end space-y-2">
+          <a href={`https://instagram.com/${info.social.instagram}`} className="-ft-3" target="_blank">Instagram</a>
+          <a href={`https://facebook.com/${info.social.facebook}`} className="-ft-3" target="_blank">Facebook</a>
+          <a href={`https://wa.me/${info.whatsapp.value}`} className="-ft-3" target="_blank">WhatsApp</a>
+        </div>
+      </div>
+
+      <div className="fixed top-0 inset-x-0 flex lg:hidden justify-between bg-white border-b p-4 z-[9999]">
+        <Link href="/">
+          <div id="logo" className="relative flex justify-start w-[16rem] pt-[4rem] top-0">
+            <Img
+              src={logotype}
+              fill={true}
+              style={{objectFit: 'contain', filter: 'brightness(0)'}}
+            />
+          </div>
+        </Link>
+        {/*<div className="w-[4rem] h-[4rem] cursor-pointer" onClick={() => setOpenBurger(!openBurger)}>*/}
+        {/*  <a*/}
+        {/*    className={`burger_box w-10 h-8 ${openBurger && 'active'}`}*/}
+        {/*  >*/}
+        {/*    <span className="burger_ico"/>*/}
+        {/*  </a>*/}
+        {/*</div>*/}
+      </div>
+    </>
+  );
 
 }

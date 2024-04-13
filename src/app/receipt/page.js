7bdadfae66f47bfbe741142bbcbe23d79408e6ee
Adam Black <fernando@notoriovs.com>
2024-04-12 22:10:26 -0600

    UPD: Styles for desktop Receipt

--- src/app/receipt/page.js
+++ src/app/receipt/page.js
@@ -13,9 +13,9 @@ export default function Receipt() {
 
   function formatDate(value = new Date, separator = '-', addedDays = 0) {
     let date = new Date(value);
-    const day = (date.getDate() + addedDays).toLocaleString('es-MX', { day: '2-digit' });
-    const month = date.toLocaleString('es-MX', { month: '2-digit' });
-    const year = date.toLocaleString('es-MX', { year: 'numeric' });
+    const day = (date.getDate() + addedDays).toLocaleString('es-MX', {day: '2-digit'});
+    const month = date.toLocaleString('es-MX', {month: '2-digit'});
+    const year = date.toLocaleString('es-MX', {year: 'numeric'});
     return year + separator + month + separator + day;
   }
 
@@ -35,7 +35,7 @@ export default function Receipt() {
     issueDate: formatDate(date, '-'),
     dueDate: formatDate(date, '-', 7),
     client: client,
-    refCode: formatDate(date, '') + '-' + client.replace(/[aeiou]/g, '').substring(0, 2) + '-' + concept.substring(0,2),
+    refCode: formatDate(date, '') + '-' + client.replace(/[aeiou]/g, '').substring(0, 2) + '-' + concept.substring(0, 2),
     concept: searchParams.get('concept'),
     amount: () => {
       return formatCurr.format(amount);
@@ -65,76 +65,78 @@ export default function Receipt() {
     return () => window.removeEventListener('resize', setArrowSizes);
   }, [borderWidth]);
 
-  return (<div className="bg-gray-50 flex flex-col">
-    <div className="flex">
-      <div className="max-w-[9rem] w-1/12 h-0 border-t-black border-l-transparent border-r-transparent"
-           style={{borderTopWidth: borderHeight, borderLeftWidth: borderWidth, borderRightWidth: borderWidth}}></div>
-      <div className="max-w-[9rem] w-1/12 h-0 border-t-black border-l-transparent border-r-transparent"
-           style={{borderTopWidth: borderHeight, borderLeftWidth: borderWidth, borderRightWidth: borderWidth}}></div>
-      <div className="max-w-[9rem] w-1/12 h-0 border-t-black border-l-transparent border-r-transparent"
-           style={{borderTopWidth: borderHeight, borderLeftWidth: borderWidth, borderRightWidth: borderWidth}}></div>
-      <div className="max-w-[9rem] w-1/12 h-0 border-t-black border-l-transparent border-r-transparent"
-           style={{borderTopWidth: borderHeight, borderLeftWidth: borderWidth, borderRightWidth: borderWidth}}></div>
-      <div className="max-w-[9rem] w-1/12 h-0 border-t-black border-l-transparent border-r-transparent"
-           style={{borderTopWidth: borderHeight, borderLeftWidth: borderWidth, borderRightWidth: borderWidth}}></div>
-      <div className="max-w-[9rem] w-1/12 h-0 border-t-black border-l-transparent border-r-transparent"
-           style={{borderTopWidth: borderHeight, borderLeftWidth: borderWidth, borderRightWidth: borderWidth}}></div>
-      <div className="max-w-[9rem] w-1/12 h-0 border-t-black border-l-transparent border-r-transparent"
-           style={{borderTopWidth: borderHeight, borderLeftWidth: borderWidth, borderRightWidth: borderWidth}}></div>
-      <div className="max-w-[9rem] w-1/12 h-0 border-t-black border-l-transparent border-r-transparent"
-           style={{borderTopWidth: borderHeight, borderLeftWidth: borderWidth, borderRightWidth: borderWidth}}></div>
-      <div className="max-w-[9rem] w-1/12 h-0 border-t-black border-l-transparent border-r-transparent"
-           style={{borderTopWidth: borderHeight, borderLeftWidth: borderWidth, borderRightWidth: borderWidth}}></div>
-      <div className="max-w-[9rem] w-1/12 h-0 border-t-black border-l-transparent border-r-transparent"
-           style={{borderTopWidth: borderHeight, borderLeftWidth: borderWidth, borderRightWidth: borderWidth}}></div>
-      <div className="max-w-[9rem] w-1/12 h-0 border-t-black border-l-transparent border-r-transparent"
-           style={{borderTopWidth: borderHeight, borderLeftWidth: borderWidth, borderRightWidth: borderWidth}}></div>
-      <div className="max-w-[9rem] w-1/12 h-0 border-t-black border-l-transparent border-r-transparent"
-           style={{borderTopWidth: borderHeight, borderLeftWidth: borderWidth, borderRightWidth: borderWidth}}></div>
+  return (
+    <div className="bg-gray-50 flex flex-col">
+      <div className="flex">
+        <div className="max-w-[9rem] w-1/12 h-0 border-t-black border-l-transparent border-r-transparent"
+             style={{borderTopWidth: borderHeight, borderLeftWidth: borderWidth, borderRightWidth: borderWidth}}></div>
+        <div className="max-w-[9rem] w-1/12 h-0 border-t-black border-l-transparent border-r-transparent"
+             style={{borderTopWidth: borderHeight, borderLeftWidth: borderWidth, borderRightWidth: borderWidth}}></div>
+        <div className="max-w-[9rem] w-1/12 h-0 border-t-black border-l-transparent border-r-transparent"
+             style={{borderTopWidth: borderHeight, borderLeftWidth: borderWidth, borderRightWidth: borderWidth}}></div>
+        <div className="max-w-[9rem] w-1/12 h-0 border-t-black border-l-transparent border-r-transparent"
+             style={{borderTopWidth: borderHeight, borderLeftWidth: borderWidth, borderRightWidth: borderWidth}}></div>
+        <div className="max-w-[9rem] w-1/12 h-0 border-t-black border-l-transparent border-r-transparent"
+             style={{borderTopWidth: borderHeight, borderLeftWidth: borderWidth, borderRightWidth: borderWidth}}></div>
+        <div className="max-w-[9rem] w-1/12 h-0 border-t-black border-l-transparent border-r-transparent"
+             style={{borderTopWidth: borderHeight, borderLeftWidth: borderWidth, borderRightWidth: borderWidth}}></div>
+        <div className="max-w-[9rem] w-1/12 h-0 border-t-black border-l-transparent border-r-transparent"
+             style={{borderTopWidth: borderHeight, borderLeftWidth: borderWidth, borderRightWidth: borderWidth}}></div>
+        <div className="max-w-[9rem] w-1/12 h-0 border-t-black border-l-transparent border-r-transparent"
+             style={{borderTopWidth: borderHeight, borderLeftWidth: borderWidth, borderRightWidth: borderWidth}}></div>
+        <div className="max-w-[9rem] w-1/12 h-0 border-t-black border-l-transparent border-r-transparent"
+             style={{borderTopWidth: borderHeight, borderLeftWidth: borderWidth, borderRightWidth: borderWidth}}></div>
+        <div className="max-w-[9rem] w-1/12 h-0 border-t-black border-l-transparent border-r-transparent"
+             style={{borderTopWidth: borderHeight, borderLeftWidth: borderWidth, borderRightWidth: borderWidth}}></div>
+        <div className="max-w-[9rem] w-1/12 h-0 border-t-black border-l-transparent border-r-transparent"
+             style={{borderTopWidth: borderHeight, borderLeftWidth: borderWidth, borderRightWidth: borderWidth}}></div>
+        <div className="max-w-[9rem] w-1/12 h-0 border-t-black border-l-transparent border-r-transparent"
+             style={{borderTopWidth: borderHeight, borderLeftWidth: borderWidth, borderRightWidth: borderWidth}}></div>
+      </div>
+      <div className="w-full px-8 md:px-40 flex-grow mono border-b">
+        <div className="flex flex-col md:flex-row border-b py-20">
+          <div className="w-1/3 mb-12 md:mb-0 md:w-1/6 relative mx-auto flex flex-col justify-center items-center">
+            <Image src={LogoSymbol} alt="Logo" className="w-2/3 mb-6"/>
+            <Image src={Logotype} alt="Logo" className="w-full"/>
+          </div>
+          <div className="flex-grow flex justify-center md:justify-end md:items-end">
+            <p className="!leading-[1.5] text-center md:text-right">Bar code services S.A. de C.V.<br/>
+              Montreal 1071, Providencia 2a Secc.<br/>
+              Guadalajara, Jal.<br/>
+              Tel: +52 33 1080 4567
+            </p>
+          </div>
+        </div>
+        <div className="py-20 grid grid-cols-2">
+          <p className="!leading-[1.5] text-left">Fecha emisión:</p>
+          <p className="!leading-[1.5] text-right">{receiptInfo.issueDate}</p>
+          <p className="!leading-[1.5] text-left">Cliente:</p>
+          <p className="!leading-[1.5] text-right">{receiptInfo.client}</p>
+          <p className="!leading-[1.5] text-left">Referencia:</p>
+          <p
+            className="!leading-[1.5] text-right uppercase">{`PAY-${receiptInfo.refCode}`}</p>
+        </div>
+        <div className="grid pb-8 grid-cols-2 border-dashed border-b-2 border-black">
+          <p className="!leading-[1.5] font-bold text-left uppercase italic">Concepto</p>
+          <p className="!leading-[1.5] font-bold text-right uppercase italic">Monto</p>
+        </div>
+        <div className="py-12 grid grid-cols-2 border-dashed border-b-2 border-black">
+          <p className="!leading-[1.5] text-left">{receiptInfo.concept}</p>
+          <p className="!leading-[1.5] text-right">{receiptInfo.amount()}</p>
+        </div>
+        <div className="py-12 grid grid-cols-2">
+          <p className="!leading-[1.5] text-left">IVA:</p>
+          <p className="!leading-[1.5] text-right">{receiptInfo.vat().value}</p>
+          <p className="!leading-[1.5] text-left">A pagar:</p>
+          <p className="!leading-[1.5] text-right">{receiptInfo.vat().totalDue}</p>
+          <p className="!leading-[1.5] text-left">Fecha límite pago:</p>
+          <p className="!leading-[1.5] text-right">{receiptInfo.dueDate}</p>
+        </div>
+        <div className="py-12">
+          <p className="!leading-[1.5] text-center">Gracias</p>
+        </div>
+      </div>
+      <p className="-ft-3 p-8 md:p-20">{receiptInfo.id}</p>
     </div>
-    <div className="w-full pt-60 px-40 flex-grow mono">
-      <div className="w-1/3 relative mb-40 mx-auto flex flex-col justify-center items-center">
-        <Image src={LogoSymbol} alt="Logo" className="w-2/3 mb-6"/>
-        <Image src={Logotype} alt="Logo" className="w-full"/>
-      </div>
-      <div className="mb-40">
-        <p className="ft-6 !leading-[1.5] text-center">Bar code services S.A. de C.V.<br/>
-          Montreal 1071, Providencia 2a Secc.<br/>
-          Guadalajara, Jal.<br/>
-          Tel: +52 33 1080 4567
-        </p>
-      </div>
-      <div className="mb-40 grid grid-cols-2">
-        <p className="ft-6 !leading-[1.5] text-left">Fecha emisión:</p>
-        <p className="ft-6 !leading-[1.5] text-right">{receiptInfo.issueDate}</p>
-        <p className="ft-6 !leading-[1.5] text-left">Cliente:</p>
-        <p className="ft-6 !leading-[1.5] text-right">{receiptInfo.client}</p>
-        <p className="ft-6 !leading-[1.5] text-left">Referencia:</p>
-        <p
-          className="ft-6 !leading-[1.5] text-right uppercase">{`PAY-${receiptInfo.refCode}`}</p>
-      </div>
-      <div className="grid py-12 grid-cols-2 border-dashed border-b-2 border-black">
-        <p className="ft-6 !leading-[1.5] font-bold text-left uppercase italic">Concepto</p>
-        <p className="ft-6 !leading-[1.5] font-bold text-right uppercase italic">Monto</p>
-      </div>
-      <div className="py-12 grid grid-cols-2 border-dashed border-b-2 border-black">
-        <p className="ft-6 !leading-[1.5] text-left">{receiptInfo.concept}</p>
-        <p className="ft-6 !leading-[1.5] text-right">{receiptInfo.amount()}</p>
-      </div>
-      <div className="mb-20 py-12 grid grid-cols-2">
-        <p className="ft-6 !leading-[1.5] text-left">IVA:</p>
-        <p className="ft-6 !leading-[1.5] text-right">{receiptInfo.vat().value}</p>
-        <p className="ft-6 !leading-[1.5] text-left">A pagar:</p>
-        <p className="ft-6 !leading-[1.5] text-right">{receiptInfo.vat().totalDue}</p>
-      </div>
-      <div className="mb-20 py-12 grid grid-cols-2">
-        <p className="ft-6 !leading-[1.5] text-left">Fecha límite pago:</p>
-        <p className="ft-6 !leading-[1.5] text-right">{receiptInfo.dueDate}</p>
-      </div>
-      <div>
-        <p className="ft-6 !leading-[1.5] text-center">Gracias</p>
-      </div>
-    </div>
-    <p className='p-20'>{receiptInfo.id}</p>
-  </div>);
+  );
 }
